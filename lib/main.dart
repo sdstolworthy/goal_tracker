c1ddba4bc9f92271c4d02dfbcc3fde66116d5a6e
Spencer Stolworthy <sdstolworthy@gmail.com>
2020-01-03 23:56:17 -0700

    refactor navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:spencerstolworthy_goals/src/FlutterApp.dart';
 
-void main() => runApp(FlutterApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(FlutterApp());
+}
